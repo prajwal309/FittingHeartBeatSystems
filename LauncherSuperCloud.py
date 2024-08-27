bfb3c496bb7d9186925737c924661005c9a73042
prajwal309 <prajwalniraula@gmail.com>
2024-08-27 13:21:47 -0400

    Added targets from my table.

--- LauncherSuperCloud.py
+++ LauncherSuperCloud.py
@@ -1,6 +1,7 @@
 import numpy as np
 import glob
 import os
+import pandas as pd
 
 #Ask for more nodes so this runs much faster.
 BaseText = "#!/bin/bash\nsource /etc/profile\nmodule load anaconda/2022b \nexport OMP_NUM_THREADS=NUMCORES\nexport MKL_NUM_THREADS=NUMCORES\n\npython ProcessAndFitTransit.py $1 $2 \n"
@@ -19,22 +20,34 @@ Command = "LLMapReduce --mapper mapper.sh --input input.txt --np=[%s,%s,%s] --ke
 BaseText = BaseText.replace("NUMCORES", str(NumCores))
 
 #Now load for all the text files in the Generated light curves folder.
-
 fileList = glob.glob("ProcessedLightCurve/*")
 print("The number of files is:", len(fileList))
+
+#Read Diana Targets and launch with Toggle 1
+DianaTargetsList = np.loadtxt("database/SelectedWindemuthTargets.txt")
+MyTargetList = pd.read_csv("database/SelectedMyTarget.txt", delimiter="&")
+
+
+DianaKIC = DianaTargetsList #Do this with toggle 1
+MyKIC = MyTargetList["KIC"].values #Do this with toggle 0
+
+
+#Read my targets and launch with Toggle 0
 
 
 #Things you need in input.txt
 #python3 ProcessAndFitTransit.py 4851217 ProcessedLightCurve/004851217.txt
 
-for fileItem in fileList:
-    print("The name of the subfolder is given by:", fileItem)
-    #Launch the code in the triple mode
-    KICValue = str(int(fileItem.split("/")[-1].split(".")[0]))
-    print(KICValue)
+for KICValue in DianaTargetsList:
+    fileItem = "ProcessedLightCurve/"+str(int(KICValue)).zfill(9)+".txt"
     with open("input.txt", "a") as f:
-        f.write(KICValue+" "+fileItem+"\n")
-    
+        f.write(str(int(KICValue))+" "+fileItem+" 1\n")
+
+
+for KICValue in MyKIC:
+    fileItem = "ProcessedLightCurve/"+str(int(KICValue)).zfill(9)+".txt"
+    with open("input.txt", "a") as f:
+        f.write(str(int(KICValue))+" "+fileItem+" 0\n")
     
 #Now create the mapper.sh
 with open("mapper.sh", "w") as f:
